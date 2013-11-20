4ca0e431e6a448222ce4d9913b6a4ab107ae32d8
David J. Brenes <david.brenes@simplelogica.net>
2013-11-20 12:52:20 +0100

    Handle urls like http://www.youtube.com/v/ID
    Youtube URLs sometimes are like http://www.youtube.com/v/HtAXSpBM8XI and now we can handle them

--- lib/acts_as_unvlogable/vg_youtube.rb
+++ lib/acts_as_unvlogable/vg_youtube.rb
@@ -8,7 +8,10 @@ class VgYoutube
   def initialize(url=nil, options={})
     object = YouTubeIt::Client.new({})
     @url = url
+    # This line handles URL like http://www.youtube.com/watch?v=HtAXSpBM8XI
     @video_id = @url.query_param('v')
+    # This line handles URL like http://www.youtube.com/v/HtAXSpBM8XI
+    @video_id = @url.match(/www.youtube.com\/v\/(?<id>[^?]*)/)[:id] if @video_id.blank?
     begin
       @details = object.video_by(@video_id)
       raise if @details.blank?
